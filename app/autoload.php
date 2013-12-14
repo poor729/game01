9f826913bce95eb556edf32b571812513c7bbcc9
tsunefuka-k <tsunefuka-k@klab.com>
2013-12-14 18:45:50 +0900

    ディスパッチ処理を別ファイルに移動。エラー時の挙動を修正

---
+++ app/autoload.php
@@ -0,0 +1,36 @@
+<?php
+
+try {
+    // load controller
+    $class = $_GET['class'];
+    $method = $_GET['method'];
+    $controller_file = CONTROLLER_DIR . "/{$class}_controller.php";
+    if (!file_exists($controller_file)) {
+        throw new Exception("not found controller:{$controller_file}");
+    }
+    require_once($controller_file);
+
+    // execute controller
+    $class_name = '';
+    foreach (explode('_', $class) as $word) {
+        $class_name .= ucwords($word);
+    }
+    $class_name .= "Controller";
+    $controller = new $class_name();
+    if (!method_exists($controller, $method)) {
+        throw new Exception("not found method:{$class_name}/{$method}");
+    }
+    $controller->{$method}();
+    $view_file = VIEW_DIR . "/{$class}/{$method}.php";
+
+    // execute view
+    if (!file_exists($view_file)) {
+        throw new Exception("not found view:{$view_file}");
+    }
+    require_once($view_file);
+} catch(Exception $e) {
+    require_once(CONTROLLER_DIR . "/error_controller.php");
+    $error = new ErrorController();
+    $error->index();
+    require_once(VIEW_DIR . "/error/index.php");
+}
--- web/index.php
+++ web/index.php
@@ -6,19 +6,5 @@ define("CONTROLLER_DIR", APP_DIR . "/controller");
 define("VIEW_DIR", APP_DIR . "/view");
 define("CONFIG", APP_DIR . "/config");
 
-// load controller
-$class = $_GET['class'];
-$method = $_GET['method'];
-require (CONTROLLER_DIR . "/{$class}_controller.php");
-
-// execute controller
-$class_name = '';
-foreach (explode('_', $class) as $word) {
-    $class_name .= ucwords($word);
-}
-$class_name .= "Controller";
-$controller = new $class_name();
-$controller->{$method}();
-
-// execute view
-require (VIEW_DIR . "/{$class}/{$method}.php");
+// dispatcher
+require_once (APP_DIR . "/autoload.php");
